be3f5423c24e26a3eee2fd9d5b97d654371a1d7d
wlals2997 <wlals2997@naver.com>
2022-03-29 17:58:11 +0900

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import Home from './routes/Home';
 import Description from './routes/Description';
+import MovieSection from './routes/MovieSection';
+import Navbar from './routes/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Switch>
         <Route path='/movie/:id'>
           <Description />
+        </Route>
+        <Route path='/movie'>
+          <MovieSection />
         </Route>
         <Route path='/'>
           <Home />
---
+++ src/routes/Navbar.js
@@ -0,0 +1,30 @@
+import styles from './Navbar.module.css';
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFilm } from '@fortawesome/free-solid-svg-icons';
+function Navbar() {
+  return (
+    <>
+      <nav className={styles.navbar}>
+        <header className={styles.logo}>
+          <a href='/'>
+            <FontAwesomeIcon icon={faFilm} className={styles.icon} />
+            MOVIE
+          </a>
+        </header>
+        <ul className={styles.navbar_list}>
+          <li className={styles.menu}>
+            <a href='/movie'>영화</a>
+          </li>
+          <li className={styles.menu}>
+            <a href='/movie'>예매</a>
+          </li>
+          <li className={styles.menu}>
+            <a href='/movie'>스토어</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+export default Navbar;
