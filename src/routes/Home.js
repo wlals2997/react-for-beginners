6d5ac93e2e81c638ea853edf1b3b936b40d537ff
wlals2997 <wlals2997@naver.com>
2022-04-01 23:09:28 +0900

    Home.js api 호출

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,26 @@
-
-
+import { useEffect, useState } from 'react';
+import styles from '../routes/Home.module.css';
 function Home() {
- 
-  
-return(
-  <div></div>
-)
+  const [image, setImage] = useState([]);
+  const getImage = async () => {
+    const response = await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+    );
+    const json = await response.json();
+    setImage(json.data.movies);
+  };
+  useEffect(() => {
+    getImage();
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      {image.map((img, key) => (
+        <div key={img.id} className={styles.box}>
+          <img src={img.medium_cover_image} alt='Main_image'></img>
+        </div>
+      ))}
+    </div>
+  );
 }
 export default Home;
